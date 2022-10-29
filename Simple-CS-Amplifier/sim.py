a64cd038bfd0188760e1a01a3c86ce382d7d6c69
emmanestallo <erestallo@up.edu.ph>
2022-10-29 22:38:13 +0800

    simple cs amp

--- Simple-CS-Amplifier/sim.py
+++ Simple-CS-Amplifier/sim.py
@@ -1,21 +1,21 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-'''
-lengths = ['0.15','0.20','0.25','0.30','0.35','0.40']
-for length in lengths: 
-    vds = np.loadtxt(f'ao-L={length}.txt')[:,0]
-    ao = np.loadtxt(f'ao-L={length}.txt')[:,1] 
-    plt.plot(vds,ao,label=length) 
 
-plt.title('nfet_01v8_lvt $(W=40\mu m, I_D = 1.0 mA)$')
+lengths = ['0.20','0.21','0.22','0.23','0.24','0.25']
+for length in lengths: 
+    ft = np.loadtxt(f'charac-L={length}.txt')[:,1]/1e9
+    vstar = np.loadtxt(f'charac-L={length}.txt')[:,5] 
+    plt.plot(vstar,ft,label=length,ncol=3) 
+
+plt.title('nfet_01v8_lvt $(W=99.9\mu m, V_{DS} = 0.7mV)$')
 plt.legend(loc='lower right',ncol=3)
-plt.ylabel('$a_0 = g_mr_o$')
-plt.xlabel('$V_{DS}$')
+plt.ylabel('$f_t$ [GHz]')
+plt.xlabel('$V^* [V]$')
 plt.grid(linestyle='--')
 plt.show()
-''' 
 
+'''
 vstar = np.loadtxt(f'vstar.txt')[:,1]
 id = np.loadtxt(f'vstar.txt')[:,3]
 
@@ -33,3 +33,4 @@ plt.ylabel('$I_D \; [mA]$')
 plt.xlabel('$V^* \; [V]$')
 plt.grid(linestyle='--')
 plt.savefig('vstar-id.png',dpi=600)
+'''
